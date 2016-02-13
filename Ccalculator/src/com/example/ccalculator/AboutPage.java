0493ebf090b8d2ac53414d2ee0f9fc6ede4d2512
Abnerlvx <250576985@qq.com>
2016-02-13 18:27:50 +0800

    新增项目

---
+++ Ccalculator/src/com/example/ccalculator/AboutPage.java
@@ -0,0 +1,16 @@
+package com.example.ccalculator;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_page);
+	}
+	
+}
